e0a4bcd0ae025bb3542b591c7d3b5a372d67c577
Noussaiba Hadigui <noussaibahadigui5@gmail.com>
2026-01-06 06:45:38 +0100

    Initial commit

---
+++ service-client/src/main/java/com/example/serviceclient/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.example.serviceclient.repositories;
+
+import com.example.serviceclient.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
